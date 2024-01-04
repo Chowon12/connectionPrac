315c3ca5e95881085d18f88c8a7db61a671131e4
tiger9397 <tiger9397@gmail.com>
2024-01-04 11:43:57 +0900

    DeleteDetpController

--- connectionPrac/src/dept/controller/DeleteDeptController.java
+++ connectionPrac/src/dept/controller/DeleteDeptController.java
@@ -15,7 +15,41 @@ import dept.dto.Dept;
 @WebServlet("/deleteDept.do")
 public class DeleteDeptController extends HttpServlet{
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// ?
+		String url = "errors/error.jsp";
+		
+		System.out.println("DeleteDeptController");
+		int deptno = Integer.parseInt(request.getParameter("deptno"));
+		System.out.println(deptno);
+				
+		Dept dept = null;
+		boolean result = false;
+		
+		try {
+			dept = DeptDAO.getDeptByDeptno(deptno);
+			
+			if(dept == null) {
+				request.setAttribute("error", "존재하지않는부서");
+				request.getRequestDispatcher(url).forward(request, response);
+			}else {
+				result = DeptDAO.deleteDeptByDeptno(deptno);
+			}
+			
+			if(result) {
+				url = "getDeptList.do";
+				response.sendRedirect(url);
+				return;
+			}else {
+				request.setAttribute("error", "부서 삭제 실패");
+				request.getRequestDispatcher(url).forward(request, response);
+			}
+		} catch (Exception e) {
+			request.setAttribute("error", "부서 삭제 실패");
+			request.getRequestDispatcher(url).forward(request, response);
+		}
+		
+		
+		
+		
 		
 	}
 }
